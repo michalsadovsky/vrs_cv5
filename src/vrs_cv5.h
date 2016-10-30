edae82e547e7515e2da73ae3b4396e74414c7f55
michalsadovsky <m.sado018@gmail.com>
2016-10-30 23:29:31 +0100

    Update vrs_cv5.h

--- src/vrs_cv5.h
+++ src/vrs_cv5.h
@@ -8,9 +8,21 @@
 #ifndef VRS_CV5_H_
 #define VRS_CV5_H_
 
-void usart_init(void);
-void gpio_init(void);
+#include <string.h>
+#include <stddef.h>
+#include "stm32l1xx.h"
+
+int valueOVR;
+uint16_t valueEOC;
+uint16_t recivedValue;
+int format;
+uint16_t AD_value;
+char buffer[5];
+int i;
+char s[4];
+void usart_init1(void);
+void gpio_init1(void);
 int adc_getMeasurement(void);
-void adc_init(void);
+void adc_init1(void);
 
 #endif /* VRS_CV5_H_ */
